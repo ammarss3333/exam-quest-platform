2d7bdcefcf4eca29cbc442b2acb6ba3645a5067b
ammarss3333 <ammarss333@hotmail.com>
2025-10-05 16:19:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [react( )],
   base: '/exam-quest-platform/',
     define: {
-    'import.meta.env.VITE_FIREBASE_API_KEY': `''${process.env.VITE_FIREBASE_API_KEY}'`,
-    'import.meta.env.VITE_FIREBASE_AUTH_DOMAIN': `''${process.env.VITE_FIREBASE_AUTH_DOMAIN}'`,
-    'import.meta.env.VITE_FIREBASE_PROJECT_ID': `''${process.env.VITE_FIREBASE_PROJECT_ID}'`,
-    'import.meta.env.VITE_FIREBASE_STORAGE_BUCKET': `''${process.env.VITE_FIREBASE_STORAGE_BUCKET}'`,
-    'import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID': `''${process.env.VITE_FIREBASE_MESSAGING_SENDER_ID}'`,
-    'import.meta.env.VITE_FIREBASE_APP_ID': `''${process.env.VITE_FIREBASE_APP_ID}'`,
+    'import.meta.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
+    'import.meta.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
+    'import.meta.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID),
+    'import.meta.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.VITE_FIREBASE_STORAGE_BUCKET),
+    'import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
+    'import.meta.env.VITE_FIREBASE_APP_ID': JSON.stringify(process.env.VITE_FIREBASE_APP_ID),
   },
 })
